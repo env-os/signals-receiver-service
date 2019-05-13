9e1f15fb3c5fa2db6fbd53f8bfb147181a81ab82
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-13 09:59:30 +0200

    layout app.ts code with connection and mqtt

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,32 @@
 import 'reflect-metadata';
+import express from 'express'
+import mqtt from 'mqtt';
+import { useContainer, createConnection } from 'typeorm';
+import { Container } from 'typedi';
+
+
+useContainer(Container)
+
+const app: express.Application = express()
+const brokerUrl = process.env.BROKER_URL || 'mqtt://localhost';
+const topic = process.env.TOPIC || 'general';
+const port = process.env.PORT || 3000;
+
+createConnection()
+.then(async connection => {
+    console.log("Database connection started successfully")
+
+    const client = mqtt.connect(brokerUrl)
+    client.on('connect', () => {
+        client.subscribe(topic);
+    })
+
+    client.on('message', (topic, message) => {
+        console.log(message);
+    })
+
+    app.listen(port, () => {
+        console.log("Signal receiver service listening on port " + port)
+    })
+})
+.catch(error => console.log(error))
