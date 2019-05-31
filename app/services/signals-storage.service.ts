9a7235321bc2b129239d7414358de4d172b1deda
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-31 12:19:49 +0200

    Split method for convert message to signal, return promise and valuate it

--- app/services/signals-storage.service.ts
+++ app/services/signals-storage.service.ts
@@ -2,7 +2,6 @@ import { Service } from 'typedi'
 import { InjectRepository } from 'typeorm-typedi-extensions'
 import { Signal } from '../entities/signal.entity';
 import { SignalRepository } from '../repositories/signal.repository';
-import { Place } from '../entities/place.entity';
 
 @Service()
 export class SignalReceiverService {
@@ -11,21 +10,44 @@ export class SignalReceiverService {
         private readonly signals: SignalRepository,
     ) {}
 
-    private convertMessageToSignal(message: Buffer): Signal{
-        const messageJSON = JSON.parse(message.toString());
-        return new Signal(
-            messageJSON['deviceId'],
-            messageJSON['data'],
-            messageJSON['sendDateTime'],
-            new Place(
-                messageJSON['latitude'],
-                messageJSON['longitude'],
-            )
-        )
+    private async getJsonByBuffer(message: Buffer): Promise<JSON>{
+        return new Promise<JSON>((resolve, reject) => {
+            try {
+                resolve(
+                    JSON.parse(message.toString())
+                )
+            } catch(SyntaxError){
+                reject(SyntaxError)
+            }
+        });
+    }
+
+    private async getSignalByJson(json: any): Promise<Signal>{
+        return new Promise<Signal>((resolve, reject) => {
+            resolve(new Signal(
+                json.deviceId,
+                json.data,
+                json.sendDateTime,
+                json.latitude,
+                json.longitude,
+            ))
+        })
     }
 
     public async save(message: Buffer){
-        const signal = this.convertMessageToSignal(message);
-        await this.signals.save(signal);
+        await this.getJsonByBuffer(message).then(async (json) => {
+            await this.getSignalByJson(json).then(async (signal) => {
+                await this.signals.save(signal)
+                .then(() => {
+                    console.log("Signal saved.");
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        });
     }
 }
